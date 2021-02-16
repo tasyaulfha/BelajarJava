d4892f8aa99a984a581965a493f77d5fa22c0d34
tasyaulfha <17523184@students.uii.ac.id>
2021-02-16 21:13:49 +0700

    Input Output java

---
+++ InputOutput/src/com/dicoding/javafundamental/inputoutput/InputOutputFunction.java
@@ -0,0 +1,16 @@
+package com.dicoding.javafundamental.inputoutput;
+
+import java.util.Scanner;
+
+public class InputOutputFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Program penjumalahan sangat sederhana");
+        System.out.print("Masukan Angka pertama : ");
+        int value = scanner.nextInt();
+        System.out.print("Masukan Angka kedua : ");
+        int anotherValue = scanner.nextInt();
+        int result = value + anotherValue;
+        System.out.println("Hasilnya adalah : " + result);
+    }
+}
