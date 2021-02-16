4752282476462adb75f37e001eda995af6439300
tasyaulfha <17523184@students.uii.ac.id>
2021-02-16 20:46:09 +0700

    Struktur data

---
+++ StrukturDasar/src/com/dicoding/javafundamental/basic/Kendaraan/Kereta.java
@@ -0,0 +1,7 @@
+package com.dicoding.javafundamental.basic.Kendaraan;
+
+public class Kereta {
+    public static void jumlahBan() {
+        System.out.println("Ban kereta banyak!");
+    }
+}
---
+++ StrukturDasar/src/com/dicoding/javafundamental/basic/Kendaraan/Mobil.java
@@ -0,0 +1,7 @@
+package com.dicoding.javafundamental.basic.Kendaraan;
+
+public class Mobil {
+    public static void jumlahBan(){
+        System.out.println("Ban mobil 4");
+    }
+}
